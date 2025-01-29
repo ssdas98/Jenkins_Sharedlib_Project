d78de4aa9dc000fef86131b5783a8ea8586c3449
ssdas98 <ssdas181298@gmail.com>
2025-01-29 23:40:53 +0530

    Update Helper.groovy

--- src/org/mycompany/pipeline/Helper.groovy
+++ src/org/mycompany/pipeline/Helper.groovy
@@ -1,6 +1,9 @@
 package org.mycompany.pipeline
 
 class Helper {
+  static void echo(String message) {
+        println message
+    }
   static void build(String Name) {
     echo "Building project : ${Name}"
   }
